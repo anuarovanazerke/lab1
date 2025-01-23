3d92733348ab215d2faab84057b6695b861d9b80
Nazerke Anuarova <nazerke.anuarova@icloud.com>
2025-01-23 15:12:04 +0500

    lab1

---
+++ pycom.py
@@ -0,0 +1,9 @@
+#this is a comment
+
+
+#this is a comment 
+#wrinnen in
+#more than just one line
+
+
+"""This is a comment no"""
---
+++ pymod.py
@@ -0,0 +1,15 @@
+a="Hello,World!"
+print(a.upper())
+
+#Same lowercase 
+
+"""a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+"""
